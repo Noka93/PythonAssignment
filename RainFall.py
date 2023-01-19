ee14617cf95edf03ab1984e196be83d847fac7bd
Noka93 <99343159+Noka93@users.noreply.github.com>
2023-01-19 18:51:14 +0100

    update rainfall

--- RainFall.py
+++ RainFall.py
@@ -1,5 +1,4 @@
 
-
 inch_str = input("Enter an inch of rain fall: ")
 
 inch_str = int(inch_str)
@@ -7,8 +6,6 @@ inch_str = int(inch_str)
 volume = float(inch_str/0.623)
 
 gallons = volume * 231
-
-gallons = volume * inch_str
 
 print("The total gallon of water is ", gallons)
 
